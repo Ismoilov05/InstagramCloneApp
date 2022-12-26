76c8909e0c0af185568aba9332d9b51f27db6dca
Ismoilov05 <mqodir456@gmail.com>
2022-12-26 17:57:54 +0500

    feat: New Ui was built

--- InstagramCloneApp/Views/Cells/Home Feed/UserFollowTableViewCell.swift
+++ InstagramCloneApp/Views/Cells/Home Feed/UserFollowTableViewCell.swift
@@ -15,6 +15,8 @@ class UserFollowTableViewCell: UITableViewCell {
     public weak var delegate: UserFollowTableViewCellDelegate?
     private let profileImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.backgroundColor = .secondarySystemBackground
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
@@ -22,16 +24,19 @@ class UserFollowTableViewCell: UITableViewCell {
         let label = UILabel()
         label.numberOfLines = 1
         label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.text = "MuhammadQodir"
         return label
     }()
     private let usernameLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 1
         label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.text = "@Ismoilov.0505"
         return label
     }()
     private let followButton: UIButton = {
         let followButton = UIButton()
+        followButton.backgroundColor = .link
         return followButton
     }()
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -58,6 +63,30 @@ class UserFollowTableViewCell: UITableViewCell {
     }
     override func layoutSubviews() {
         super.layoutSubviews()
+        profileImageView.frame = CGRect(x: 3,
+                                        y: 3,
+                                        width: contentView.height-6,
+                                        height: contentView.height-6)
+        profileImageView.layer.cornerRadius = profileImageView.height/2.0
+ 
+        
+        let  buttonWith = contentView.width > 500 ? 220.0 : contentView.width/3
+        followButton.frame =  CGRect(x: contentView.width-5-buttonWith,
+                                     y: 5,
+                                     width: buttonWith,
+                                     height: contentView.height-10)
+        
+        let labelHeight =  contentView.height/2
+        nameLabel.frame = CGRect(x: profileImageView.right+5,
+                                 y: 0,
+                                 width: contentView.width-3-profileImageView.width-buttonWith,
+                                 height: labelHeight)
+        usernameLabel.frame = CGRect(x: profileImageView.right+5,
+                                     y: nameLabel.bottom,
+                                     width: contentView.width-3-profileImageView.width-buttonWith,
+                                     height: labelHeight)
+        
+        
         
     }
     required init?(coder: NSCoder) {
